403ba528e3ccaf3f8ded0938d77904bf8e5bbf2f
jlyon87 <jml6487@gmail.com>
2017-08-05 10:25:07 -0400

    add champion carousel

---
+++ assets/js/dnd-data.js
@@ -0,0 +1,72 @@
+var App = (function DndData(App) {
+	var dwarfPaladin = {};
+	var halfOrcBarbarian = {};
+	var elfWizard = {};
+	var halflingRogue = {};
+	var humanBarbarian = {};
+
+	var App.data = [
+		{
+			id: "0",
+			name: "",
+			race: "Dwarf",
+			class: "Paladin",
+			img: "",
+			desc: "",
+			attackPower: 1,
+			defense: 1,
+			hp: 10,
+			counterAttack: 1,
+		},
+		{
+			id: "1",
+			name: "",
+			race: "Half-Orc",
+			class: "Barbarian",
+			img: "",
+			desc: "",
+			attackPower: 1,
+			defense: 1,
+			hp: 10,
+			counterAttack: 1,
+		},
+		{
+			id: "2",
+			name: "",
+			race: "Elf",
+			class: "Wizard",
+			img: "",
+			desc: "",
+			attackPower: 1,
+			defense: 1,
+			hp: 10,
+			counterAttack: 1,
+		},
+		{
+			id: "3",
+			name: "",
+			race: "Halfling",
+			class: "Rogue",
+			img: "",
+			desc: "",
+			attackPower: 1,
+			defense: 1,
+			hp: 10,
+			counterAttack: 1,
+		},
+		{
+			id: "4",
+			name: "",
+			race: "Human",
+			class: "Fighter",
+			img: "",
+			desc: "",
+			attackPower: 1,
+			defense: 1,
+			hp: 10,
+			counterAttack: 1,
+		},
+	];
+
+	return App.data;
+}(App || {}));
